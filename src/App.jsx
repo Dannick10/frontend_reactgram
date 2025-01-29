0c05691cfaacad0ffc381e5cd2db7d9f23b9c5cf
Dannick010 <danmaggot2@gmail.com>
2025-01-29 17:31:12 -0300

    adicionando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Register from './pages/auth/Register'
+import Login from './pages/auth/login'
 
 function App() {
 
   return (
     <>
-      <h1>ReactGRAAM</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
