ba76c477c14c68ba209a57a30b2aaf68059e9fb5
Dannick010 <danmaggot2@gmail.com>
2025-01-29 18:08:17 -0300

    formulario

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
+//components
+import { Link } from 'react-router-dom'
+
+
+// Hooks 
+import { useState, useEffect } from 'react'
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+
+}
+
 const Register = () => {
   return (
-    <div>Register</div>
+    <div>
+      <h2>ReactGram</h2>
+      <p className='subtitle'>Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='nome' /> 
+        <input type='email' placeholder='Email' />
+        <input type="password" placeholder='Senha' />
+        <input type='submit' placeholder='Cadastrar' />
+      </form>
+      <p>
+        Já tem conta ? <Link to={'/login'}>Ir para o login</Link>
+      </p>
+    </div>
   )
 }
 
