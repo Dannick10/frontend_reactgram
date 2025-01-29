1b8ccd116975dbcfe111ae0a9fe03d6efd6f2ad3
Dannick010 <danmaggot2@gmail.com>
2025-01-29 17:52:13 -0300

    styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,18 +12,24 @@ import { CiSearch } from "react-icons/ci";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav id="navbar">
       <Link to="/">ReactGram</Link>
-      <form>
+      <form id="search-form">
         <CiSearch />
         <input type="text" />
       </form>
       <ul id="nav-links">
-        <NavLink to="/">
-          <BsHouseDoorFill />
-        </NavLink>
-        <NavLink to="/login">Entrar</NavLink>
-        <NavLink to="/register">Cadastrar</NavLink>
+        <li>
+          <NavLink to="/">
+            <BsHouseDoorFill />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Entrar</NavLink>
+        </li>
+        <li>
+          <NavLink to="/register">Cadastrar</NavLink>
+        </li>
       </ul>
     </nav>
   );
