b75849429c477aba5340db276a705578d49efd50
Dannick010 <danmaggot2@gmail.com>
2025-01-30 20:07:58 -0300

    configurando redux toolkit

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import authReducer from './slices/authSlices'
+
 export const store = configureStore({
     reducer: {
-
+        auth: authReducer
     },
 })
