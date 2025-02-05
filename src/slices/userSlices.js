63d290df0db4e58292fec8fa70ec5bf7593e1549
Dannick010 <danmaggot2@gmail.com>
2025-02-05 14:38:02 -0300

    slices user

--- src/slices/userSlices.js
+++ src/slices/userSlices.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import userService from "../services/userService";
 
 const initialState = {
     user: {},
@@ -8,6 +9,17 @@ const initialState = {
     message: null
 }
 
+export const profile = createAsyncThunk(
+    "user/profile",
+    async(user,thunkAPI) => {
+
+        const token = thunkAPI.getState().auth.user.token 
+
+        const data = await userService.profile(user, token)
+
+        return data
+    }
+)
 
 export const userSlice = createSlice({
     name: "user",
@@ -16,7 +28,26 @@ export const userSlice = createSlice({
         resetMessage: (state) => {
             state.message = null
         }
-    }
+    },
+    extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.loading = false;
+        state.sucess = true;
+        state.error = null;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.user = null;
+        localStorage.removeItem("user");
+      })
+    } 
 })
 
 
