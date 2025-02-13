94f78ee93396b78b2525387a87e4066465481bc4
Dannick010 <danmaggot2@gmail.com>
2025-02-13 18:46:56 -0300

    feat/config reduce

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from './slices/authSlices'
 import userReduce from "./slices/userSlices";
+import photoReduce from "./slices/photoSlice"
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
-        user: userReduce
+        user: userReduce,
+        photo: photoReduce
     },
 })
