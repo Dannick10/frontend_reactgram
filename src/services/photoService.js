837700e2cabacacb61bec8fab9b24cd54940a69b
Dannick010 <danmaggot2@gmail.com>
2025-02-14 19:36:32 -0300

    /feat photo services

--- src/services/photoService.js
+++ src/services/photoService.js
@@ -1,21 +1,36 @@
 import { api, requestConfig } from "../utils/config";
 
 const publishPhoto = async (data, token) => {
-  const config = requestConfig("POST", data,token,true);
+  const config = requestConfig("POST", data, token, true);
 
   try {
     const res = await fetch(api + "/photos", config)
       .then((res) => res.json())
       .catch((err) => err);
 
-      return res
+    return res;
   } catch (err) {
     console.log(err);
   }
 };
 
+const getUserphotos = async (id) => {
+  const config = requestConfig("GET");
+
+  try {
+    const res = await fetch(api + "/photos/user/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const photoService = {
   publishPhoto,
+  getUserphotos
 };
 
 export default photoService;
